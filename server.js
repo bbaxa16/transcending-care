27f4bf48e3da678de00a8c745faadd2cb960cf22
bbaxa16 <brooke769@gmail.com>
2017-08-07 09:11:40 -0600

    started setting up heroku

--- server.js
+++ server.js
@@ -3,11 +3,20 @@ const app = express()
 const Mongoose = require('mongoose')
 const BodyParser = require('body-parser')
 const MethodOverride = require('method-override')
-const usersController = require('./controllers/users')
-//const selfcareboardsController = require('./controllers/selfcareboards')
+const session = require('express-session')
+
 //middleware
+app.use(session({
+  secret: 'hi',
+  resave: false,
+  saveUnitialized: false
+}))
+const usersController = require('./controllers/users')
 app.use('/users', usersController)
+//const selfcareboardsController = require('./controllers/selfcareboards')
 //app.use('/selfcareboards', selfcareboardsController)
+const sessionsController = require('./controllers/session.js')
+app.use('/sessions', sessionsController)
 app.use(express.static('public'))
 
 
