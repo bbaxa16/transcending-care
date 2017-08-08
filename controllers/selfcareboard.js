d604768f50a4dbf636bc90a66517b874300d217a
bbaxa16 <brooke769@gmail.com>
2017-08-08 08:41:19 -0600

    added routes to selfcare new,edit, and show pages

--- controllers/selfcareboard.js
+++ controllers/selfcareboard.js
@@ -1,9 +1,20 @@
 const express = require('express')
 const selfcareboard = express.Router()
 
-
+//Route for index to self care tiles
 selfcareboard.get('/', (req, res)=> {
   res.render('selfcare/index.ejs')
 })
-
+//Route to create a new self care tile
+selfcareboard.get('/new', (req, res)=> {
+  res.render('selfcare/new.ejs')
+})
+//Route to the show page
+selfcareboard.get('/:id', (req, res)=> {
+  res.render('selfcare/edit/ejs')
+})
+//Route to edit a self care tile.
+selfcareboard.get('/:id/edit', (req, res)=> {
+  res.render('selfcare/show.ejs')
+})
 module.exports = selfcareboard
