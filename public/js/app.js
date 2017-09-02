4baa128cf2031ff4d73e96d24ec6485f7b644af2
bbaxa16 <brooke769@gmail.com>
2017-09-02 14:51:41 -0500

    addedd functionality to show login/register forms

--- public/js/app.js
+++ public/js/app.js
@@ -2,5 +2,16 @@ const app = angular.module('transCare', [])
   angular.module('app', ['ngSanitize'])
 
 app.controller('mainController', ['$http', '$scope', function($http, $scope){
-  
+  this.loginDisplay = false;
+  this.registerDisplay = true;
+  this.toggleLogin = function(){
+    this.loginDisplay = !this.loginDisplay;
+  }
+  this.toggleRegister = function(){
+    this.registerDisplay = !this.registerDisplay;
+  }
+  this.toggleForms = function(){
+    this.loginDisplay = !this.loginDisplay;
+    this.registerDisplay = !this.registerDisplay;
+  }
 }])
