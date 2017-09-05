aa9bb8633185fddd0702877515953f83df0f9e78
bbaxa16 <brooke769@gmail.com>
2017-09-05 07:32:44 -0600

    added the forms to the html via angular

--- public/js/app.js
+++ public/js/app.js
@@ -7,8 +7,13 @@ app.controller('mainController', ['$http', '$scope', function($http, $scope){
   this.toggleLogin = function(){
     this.loginDisplay = !this.loginDisplay;
   }
-  this.toggleRegister = function(){
-    this.registerDisplay = !this.registerDisplay;
+  this.goToRegister = function(){
+    this.registerDisplay = true;
+    this.loginDisplay = false;
+  }
+  this.goToLogin = function(){
+    this.registerDisplay = false;
+    this.loginDisplay = true;
   }
   this.toggleForms = function(){
     this.loginDisplay = !this.loginDisplay;
